ed20dfd16eb0f533349f1a81a521597181a29f4d
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-05-20 12:44:47 -0400

    removed sockeito

---
+++ flask_app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import os
+
+# Flask setup
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "DefaultSecret"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# Run Flask if file is interpreted
+if __name__ == "__main__":
+    os.environ["FLASK_APP"] = "application.py"
+
+    try:
+        current = subprocess.check_output(["lsof", "-t", "-i:5000"])
+        current = max(current.decode("utf-8").split("\n"))
+        print(f"kill {current}")
+        os.system(f"kill {current}")
+    except:
+        pass
+    os.system("flask run")
