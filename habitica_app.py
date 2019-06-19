93dd6a176ff75928c8276c35c2b700b9aac7cd84
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-06-19 15:27:50 -0400

    commit

--- habitica_app.py
+++ habitica_app.py
@@ -5,12 +5,13 @@ auth_headers = {
     "x-api-key": "afe7803a-864b-4c37-9795-b347f3830f98"
 }
 
+# 1 hour habit = python
+
 def complete_habitica_todo(taskname):
     data = {
         "text": taskname,
         "type": "todo",
         "value": 1.5,
-
     }
     r = requests.post("https://habitica.com/api/v3/tasks/user",json=data, headers=auth_headers)
     data = r.json()
